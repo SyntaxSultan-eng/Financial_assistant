ebda779a32c6ef09849aaa4c76b8efafae04ad74
Vlad <vladislav5196@mail.ru>
2024-06-16 22:57:11 +0300

    Echo_bot and decouple

--- New_bot.py
+++ New_bot.py
@@ -1,4 +1,27 @@
-import aiogram
+import asyncio
+from aiogram import Bot, Dispatcher,types
+from decouple import config
+import requests
+from bs4 import BeautifulSoup
 
-API_TOKEN = "6271098507:AAE9MbgC7n2O-4JkcYsNTBO3ZH3L7AEChGM"
+############################################
 
+API_TOKEN = config('API_KEY',default = '')
+bot = Bot(API_TOKEN)
+dp = Dispatcher()
+
+############################################
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer(text=message.text)
+
+
+############################################
+
+async def main() -> None:
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
