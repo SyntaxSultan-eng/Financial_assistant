6780798179b127a0dc82cf794a5adf1d04f2d366
Vlad <vladislav5196@mail.ru>
2024-06-17 19:12:06 +0300

    add keyboard

---
+++ app/keyboard.py
@@ -0,0 +1,8 @@
+from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
+
+main_keyboard = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text = 'Курс валют(ЦБ РФ)🏛️')],
+    [KeyboardButton(text = '📋Информация')]
+], 
+    resize_keyboard= True,
+    input_field_placeholder="Выберите пункт меню.↓")
