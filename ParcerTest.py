c137edaa0a71e7f1c6195990682f078118299693
Vlad <vladislav5196@mail.ru>
2024-06-17 20:57:02 +0300

    Test_parcer dollar

--- ParcerTest.py
+++ ParcerTest.py
@@ -2,38 +2,22 @@ import requests
 from bs4 import BeautifulSoup as BS
 #import json
 
-page = requests.get("https://www.banki.ru/products/currency/cb")
-html = BS(page.content, 'lxml')
+headers = {
+    "Accept" : "*/*",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 OPR/99.0.0.0 (Edition Yx GX)"
+}
+#Дописать headers (появляется капча при парсинге)
 
-all_values = html.find("Text__sc-j452t5-0 jxxlPG")
-    
 
+url = 'https://yandex.ru/search/?text=доллар&lr=213&clid=2437996'
 
-
+page = requests.get(url,headers=headers)
+need_class = 'ConverterInfo-Rate'
+html = BS(page.content, features='lxml')
+print(html)
+all_values = html.find(class_=need_class)
 print(all_values)
-#how_much = html.find_all('tbody')[3].find("strong").text.strip()
-#print(how_much)
-#answer = ""
-#for item in all_values:
-#    item_value = item.text.strip()
-#    print(item_value)
-
-#    answer += item_value.strip() + "\n"
-#    if item_value is None:
-#        dict[str(item_name)] = None
-#        continue
-#    else:
-#        dict[str(item_name)] = "https://www.banki.ru"+str(item_value.get("href"))
-#print(answer)
-
-#dict = {
-    #"USD": "Американский Доллар",
-    #"EUR": "Евро",
-    #"CNY": "Китайский Юань",
-    #"GBP": "Британский Фунт",
-    #"CHF": "Швейцарский Франк",
-    #"JPY": "Японская Йена"
-#}
 
 
 #with open("cross-currency.json","w") as file:
